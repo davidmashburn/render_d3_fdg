10e9a41fb759c3b540ea4d62752ad5bc37c863ed
David Mashburn <david.n.mashburn@gmail.com>
2016-09-01 00:38:14 -0400

    Rearrange code into mostly-independent sections with global variables defined explicitly at the top of each, other misc cleanup

--- render_d3_fdg/render_d3_fdg.py
+++ render_d3_fdg/render_d3_fdg.py
@@ -6,13 +6,6 @@ from collections import OrderedDict
 
 from sample_data import sample_nodes, sample_links
 
-def read(f):
-    with open(f) as fid:
-        return fid.read()
-
-click_default = read('click_function_default.js.template')
-zoom_default = read('zooming_code_default.js.template')
-
 def rst(s, *repls): 
     '''Really stupid templates
        Yeah, so templates might be better. Meh.'''
@@ -65,8 +58,8 @@ def rst_file(filename, *repls):
 def render_d3_fdg(dat, scale=1, force_scale=1, default_size=5, expand_scale=3,
                   neighbor_scale=1.5, shrink_scale=1,
                   canvas_wh=(800, 800), save_freq='null',
-                  move_new_nodes_to_centroid=True, click_function=click_default,
-                  zooming_code=zoom_default, zoom_in=0.1, zoom_out=10,
+                  move_new_nodes_to_centroid=True, click_function='click_function_default',
+                  zooming_code='enable_zoom()', zoom_in=0.1, zoom_out=10,
                   js_filename='fdg.html.template'):
     move_new_nodes_to_centroid = 'true' if move_new_nodes_to_centroid else 'false'
     f = '/tmp/index.html'
@@ -200,5 +193,5 @@ if __name__ == '__main__':
     #fdg_plus_images(sample_nodes, sample_links) # saves just the image of the final render
     vals = sorted({i[2] for i in sample_links})
     sample_links = [(i, j, vals.index(k) * 1. / len(vals)) for i, j, k in sample_links]
-    fdg_plus_images(sample_nodes, sample_links, save_freq=None, js_filename='fdg_with_jquery_search_and_slider.html.template') # disabled saving, best for testing
+    fdg_plus_images(sample_nodes, sample_links, save_freq=None, js_filename='fdg_base.html.template') # disabled saving, best for testing
     
